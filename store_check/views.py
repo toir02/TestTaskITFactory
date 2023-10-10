782c9b410bf4ba152df4207666a01ab9199f6082
toir02 <wend8765@gmail.com>
2023-10-10 13:35:37 +0300

    added visit create api

--- store_check/views.py
+++ store_check/views.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
-from store_check.models import Store
-from store_check.serializers import StoreSerializer
+from store_check.models import Store, Visit
+from store_check.serializers import StoreSerializer, VisitSerializer
 
 
 class StoreListAPIView(generics.ListAPIView):
@@ -19,3 +21,25 @@ class StoreListAPIView(generics.ListAPIView):
             return queryset.none()
 
         return queryset
+
+
+class VisitCreateAPIView(generics.CreateAPIView):
+    serializer_class = VisitSerializer
+    queryset = Visit.objects.all()
+
+    def perform_create(self, serializer):
+        phone_number = self.request.query_params.get('phone_number')
+        store_pk = self.request.data.get('store_pk')
+        latitude = self.request.data.get('latitude')
+        longitude = self.request.data.get('longitude')
+
+        store = get_object_or_404(Store, pk=store_pk)
+
+        if store.employee.phone_number != phone_number:
+            raise ValidationError("Номер телефона не соответствует указанной торговой точке.")
+
+        serializer.save(latitude=latitude, longitude=longitude, store=store)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return response
