31a28f4245bffac630e0ef5f516fe832858ffdcc
toir02 <wend8765@gmail.com>
2023-10-10 09:14:39 +0300

    added models

--- store_check/models.py
+++ store_check/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Имя')
+    phone_number = models.CharField(max_length=255, verbose_name='Номер телефона')
+
+    class Meta:
+        verbose_name = 'Работник'
+        verbose_name_plural = 'Работники'
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name='Сотрудник')
+
+    class Meta:
+        verbose_name = 'Торговая точка'
+        verbose_name_plural = 'Торговые точки'
+
+
+class Visit(models.Model):
+    datetime = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время')
+    latitude = models.FloatField(verbose_name='Ширина')
+    longitude = models.FloatField(verbose_name='Долгота')
+
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, verbose_name='Торговая точка')
