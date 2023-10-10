c76b6bb964d0a2c7ad0a7a769e8339a8e65810a7
toir02 <wend8765@gmail.com>
2023-10-10 09:16:53 +0300

    added str methods in models

--- store_check/models.py
+++ store_check/models.py
@@ -5,6 +5,9 @@ class Employee(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя')
     phone_number = models.CharField(max_length=255, verbose_name='Номер телефона')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Работник'
         verbose_name_plural = 'Работники'
@@ -13,6 +16,9 @@ class Employee(models.Model):
 class Store(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name='Сотрудник')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Торговая точка'
@@ -25,3 +31,7 @@ class Visit(models.Model):
     longitude = models.FloatField(verbose_name='Долгота')
 
     store = models.ForeignKey(Store, on_delete=models.CASCADE, verbose_name='Торговая точка')
+
+    class Meta:
+        verbose_name = 'Посещение'
+        verbose_name_plural = 'Посещения'
