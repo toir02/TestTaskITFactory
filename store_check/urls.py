26134c54792acfe93344cffe2ee24e27b9a525a5
toir02 <wend8765@gmail.com>
2023-10-10 13:34:57 +0300

    added path to create visit

--- store_check/urls.py
+++ store_check/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from store_check.apps import StoreCheckConfig
-from store_check.views import StoreListAPIView
+from store_check.views import StoreListAPIView, VisitCreateAPIView
 
 app_name = StoreCheckConfig.name
 
 urlpatterns = [
-    path('stores/', StoreListAPIView.as_view(), name='stores')
+    path('stores/', StoreListAPIView.as_view(), name='stores'),
+    path('stores/create-visit/', VisitCreateAPIView.as_view(), name='create-visit'),
 ]
