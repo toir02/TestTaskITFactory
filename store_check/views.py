a3f5efb5671d7913c4bf6f3cfc409ab3c26f47fa
toir02 <wend8765@gmail.com>
2023-10-10 10:25:32 +0300

    added store list api view

--- store_check/views.py
+++ store_check/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
 
-# Create your views here.
+from store_check.models import Store
+from store_check.serializers import StoreSerializer
+
+
+class StoreListAPIView(generics.ListAPIView):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('employee__phone_number',)
