864edb440627d1e32a654ead1dd74021a12b8020
toir02 <wend8765@gmail.com>
2023-10-10 10:33:11 +0300

    added get queryset check for employee phone number

--- store_check/views.py
+++ store_check/views.py
@@ -10,3 +10,12 @@ class StoreListAPIView(generics.ListAPIView):
     queryset = Store.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('employee__phone_number',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        phone_number = self.request.query_params.get('employee__phone_number')
+
+        if not phone_number:
+            return queryset.none()
+
+        return queryset
